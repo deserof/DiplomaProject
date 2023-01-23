0094a44e64d6060e8d50363e745fe63f6fbc1a70
Artsiom Kharkevich <artsiom_kharkevich@epam.com>
2023-01-24 00:24:09 +0300

    fix

--- src/Manufacturing/Manufacturing/Manufacturing.Api/Program.cs
+++ src/Manufacturing/Manufacturing/Manufacturing.Api/Program.cs
@@ -1,20 +1,138 @@
-using Manufacturing.Api.Extensions;
+using Manufacturing.Api;
 using Manufacturing.Api.Middlewares;
+using Manufacturing.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureServices(builder.Configuration);
+
+builder.Services.AddCors();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Configure the context to use Microsoft SQL Server.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // Register the entity sets needed by OpenIddict.
+    // Note: use the generic overload if you need
+    // to replace the default OpenIddict entities.
+    options.UseOpenIddict();
+});
+
+// Register the Identity builder.Services.
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
+// (like pruning orphaned authorizations/tokens from the database) at regular intervals.
+builder.Services.AddQuartz(options =>
+{
+    options.UseMicrosoftDependencyInjectionJobFactory();
+    options.UseSimpleTypeLoader();
+    options.UseInMemoryStore();
+});
+
+// Register the Quartz.NET service and configure it to block shutdown until jobs are complete.
+builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+builder.Services.AddOpenIddict()
+
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the Entity Framework Core stores and models.
+        // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+        options.UseEntityFrameworkCore()
+               .UseDbContext<ApplicationDbContext>();
+
+        // Enable Quartz.NET integration.
+        options.UseQuartz();
+    })
+
+    // Register the OpenIddict server components.
+    .AddServer(options =>
+    {
+    // Enable the token endpoint.
+    options.SetTokenEndpointUris("connect/token");
+
+    // Enable the password flow.
+    options.AllowPasswordFlow();
+
+    // Accept anonymous clients (i.e clients that don't send a client_id).
+    options.AcceptAnonymousClients();
+
+        // Register the signing and encryption credentials.
+        options.AddEphemeralEncryptionKey()
+               .AddEphemeralSigningKey();
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options.UseAspNetCore()
+               .EnableTokenEndpointPassthrough();
+    })
+
+    // Register the OpenIddict validation components.
+    .AddValidation(options =>
+    {
+        // Import the configuration from the local OpenIddict server instance.
+        options.UseLocalServer();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+// Register the worker responsible for creating and seeding the SQL database.
+// Note: in a real world application, this step should be part of a setup script.
+builder.Services.AddHostedService<Worker>();
+
+// Swagger auth
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Manufacturing API v1.0",
+            Version = "v1"
+        });
+
+    options.AddSecurityDefinition("oauth2",
+        new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                Password = new OpenApiOAuthFlow
+                {
+                    TokenUrl = new Uri("https://localhost:7115/connect/token"),
+                }
+            }
+        });
+
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+                    {
+                        new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference{
+                                Id = "oauth2", //The name of the previously defined security scheme.
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+        });
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
