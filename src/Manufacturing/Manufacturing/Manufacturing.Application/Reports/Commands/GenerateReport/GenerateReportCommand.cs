6e08d14eef4255a7ae6f1d27ad00e15731fdef5c
Artsiom Kharkevich <artsiom_kharkevich@epam.com>
2023-06-13 10:00:28 +0300

    qwe

--- src/Manufacturing/Manufacturing/Manufacturing.Application/Reports/Commands/GenerateReport/GenerateReportCommand.cs
+++ src/Manufacturing/Manufacturing/Manufacturing.Application/Reports/Commands/GenerateReport/GenerateReportCommand.cs
@@ -3,6 +3,7 @@ using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Manufacturing.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Manufacturing.Application.Reports.Commands.GenerateReport
 {
@@ -39,22 +40,102 @@ namespace Manufacturing.Application.Reports.Commands.GenerateReport
                     Name = "Sheet1"
                 };
                 sheets.Append(sheet);
+
                 var sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+                Columns columns = new Columns();
+                uint columnCount = 6;
+                for (uint i = 1; i <= columnCount; i++)
+                {
+                    Column column = new Column
+                    {
+                        Min = i,
+                        Max = i,
+                        BestFit = true,
+                        CustomWidth = true,
+                        Width = 30
+                    };
+                    columns.Append(column);
+                }
+                worksheetPart.Worksheet.InsertBefore(columns, sheetData);
 
                 // Fill sheet
-                var row1 = new Row { RowIndex = 1 };
+
+                var products = _context.Products
+                    .Include(x => x.ProcessExecutions)
+                    .ThenInclude(x => x.ProductionProcess)
+                    .Where(x => x.CreationDate > request.From && x.CreationDate < request.End);
+                uint index = 1;
+
+                var row1 = new Row { RowIndex = index++ };
                 row1.Append(new Cell
                 {
-                    CellValue = new CellValue("Пример"),
-                    DataType = CellValues.String
+                    CellValue = new CellValue("Название"),
+                    DataType = CellValues.String,
+                    StyleIndex = 0,
                 });
-                row1.Append(new Cell { CellValue = new CellValue("Excel"), DataType = CellValues.String });
+                row1.Append(new Cell
+                {
+                    CellValue = new CellValue("Описание"),
+                    DataType = CellValues.String,
+                });
+                row1.Append(new Cell
+                {
+                    CellValue = new CellValue("Качество"),
+                    DataType = CellValues.String,
+                });
+                row1.Append(new Cell
+                {
+                    CellValue = new CellValue("Дата создания"),
+                    DataType = CellValues.String,
+                });
+                row1.Append(new Cell
+                {
+                    CellValue = new CellValue("Процессы"),
+                    DataType = CellValues.String,
+                });
                 sheetData.Append(row1);
 
-                var row2 = new Row { RowIndex = 2 };
-                row2.Append(new Cell { CellValue = new CellValue("Файл"), DataType = CellValues.String });
-                row2.Append(new Cell { CellValue = new CellValue("Скачать"), DataType = CellValues.String });
-                sheetData.Append(row2);
+                foreach (var product in products)
+                {
+                    var row = new Row {RowIndex = index++};
+                    row.Append(new Cell
+                    {
+                        CellValue = new CellValue(product.Name),
+                        DataType = CellValues.String,
+                    });
+                    row.Append(new Cell
+                    {
+                        CellValue = new CellValue(product.Description),
+                        DataType = CellValues.String,
+                    });
+                    row.Append(new Cell
+                    {
+                        CellValue = new CellValue(product.QualityStatus),
+                        DataType = CellValues.String,
+                    });
+                    row.Append(new Cell
+                    {
+                        CellValue = new CellValue(product.CreationDate.ToString("MM-dd-yyyy")),
+                        DataType = CellValues.String,
+                    });
+
+                    var processes = product.ProcessExecutions;
+                    sheetData.Append(row);
+                    foreach (var process in processes)
+                    {
+                        var nestedRow = new Row { RowIndex = index++ };
+                        nestedRow.Append(new Cell());
+                        nestedRow.Append(new Cell());
+                        nestedRow.Append(new Cell());
+                        nestedRow.Append(new Cell());
+                        nestedRow.Append(new Cell
+                        {
+                            CellValue = new CellValue(process.ProductionProcess.Name),
+                            DataType = CellValues.String,
+                        });
+                        sheetData.Append(nestedRow);
+                    }
+                }
 
                 workbookPart.Workbook.Save();
             }
